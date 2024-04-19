dba8d7f86f9d865be3fd0e2f50349c5ddec737f4
max925001 <shivam969684@gmail.com>
2024-04-19 07:22:19 -0700

    change

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -5,6 +5,7 @@ import jwt from "jsonwebtoken"
 const isLoggedIn = async (req ,res, next) =>{
 
     const {token} = req.cookies
+    console.log(token)
     if(!token){
        
         return next(new AppError('Unauthenticated please login',400))
