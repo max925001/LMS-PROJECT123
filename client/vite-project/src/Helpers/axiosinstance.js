056f4d58691343023b93a4fadbefc0481dfe4553
max925001 <shivam969684@gmail.com>
2024-04-21 11:57:25 -0700

    reomve verceljson

--- client/vite-project/src/Helpers/axiosinstance.js
+++ client/vite-project/src/Helpers/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const BASE_URL =  
- "http://localhost:5001/api/v1"
-//  "https://lms-project123-1.onrender.com/api/v1"
+//  "http://localhost:5001/api/v1"
+ "https://lms-project123-1.onrender.com/api/v1"
 
 const axiosInstance = axios.create()
 axiosInstance.defaults.baseURL = BASE_URL
