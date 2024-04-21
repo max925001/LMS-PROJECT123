93fd8f847977300eafbbae0946499004452d2cdc
max925001 <shivam969684@gmail.com>
2024-04-21 12:50:42 -0700

    adde siteName

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,7 +10,7 @@ const cookieOptions = {
     maxAge: 7*24*60*60*1000,
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production' ? true : false,
-    
+    sameSite:'None'
 
 }
 const register =async (req ,res ,next) =>{
