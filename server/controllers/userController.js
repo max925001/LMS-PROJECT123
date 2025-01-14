0a1840a6b983c92b265d50bac841aa4fae1113f3
max925001 <shivam969684@gmail.com>
2025-01-14 22:39:46 +0530

    changes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,8 +10,8 @@ const cookieOptions = {
     maxAge: 7*24*60*60*1000,
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production' ? true : false,
-    // sameSite:'None',
-    // secure: true
+    sameSite:'None',
+    secure: true
 
 }
 const register =async (req ,res ,next) =>{
